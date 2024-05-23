fe54a71bd0e43367fe8fdd7f46ad1a3c21c4d98b
leo49222 <107844648+leo49222@users.noreply.github.com>
2024-05-23 19:56:25 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById('searchButton').addEventListener('click', function() {
+    const query = document.getElementById('searchInput').value;
+    alert('You searched for: ' + query);
+});
+
+document.getElementById('learnMoreButton').addEventListener('click', function() {
+    alert('What would you like to learn about!');
+});
+
+document.getElementById('getStartedButton').addEventListener('click', function() {
+    alert('not yet available!');
+});
